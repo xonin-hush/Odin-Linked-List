251c6cc957d54180c2040bf2a0ba3986657a046c
xonin-hush <xonindev@gmail.com>
2024-06-16 16:37:15 +0100

    Initialize repo

---
+++ script.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor(value,next)
+    {
+        this.value=null
+        this.value=null
+    }
+}
+class LinkedList{
+    constructor(head){
+        this.head=null
+    }
+    
+}
