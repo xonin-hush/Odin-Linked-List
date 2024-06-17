bb0033886f345607ca1e396093ea1ef572ce71ed
xonin-hush <xonindev@gmail.com>
2024-06-17 17:05:03 +0100

    Add showTail()

--- script.js
+++ script.js
@@ -59,17 +59,26 @@ class LinkedList {
         i++;
         temp = temp.next;
       }
-      console.log("Size is: ",i);
+      console.log("Size is: ", i);
     }
   }
-  showHead(){
-    if(this.head==null){
-      console.log("list is empty")
+  showHead() {
+    if (this.head == null) {
+      console.log("list is empty");
+    } else {
+      console.log("Head is: ", this.head.value);
     }
-    else{
-      console.log("Head is: ",this.head.value)
+  }
+  showTail() {
+    if (this.head == null) {
+      console.log("list is empty");
+    } else {
+      let temp = this.head;
+      while (temp.next != null) {
+        temp = temp.next;
+      }
+      console.log("Tail is :", temp.value);
     }
-
   }
 }
 
@@ -81,5 +90,6 @@ L.prepend(22);
 L.prepend(84);
 L.prepend(33);
 L.size();
-L.showHead()
+L.showHead();
+L.showTail();
 // L.printAll();
