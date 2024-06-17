3badb721541ef926b5646564b4edf248e0f60cfe
xonin-hush <xonindev@gmail.com>
2024-06-17 16:45:05 +0100

    Add size() printAll()

--- script.js
+++ script.js
@@ -26,24 +26,53 @@ class LinkedList {
   //       }
   //     }
   //   }
+  printAll() {
+    if (this.head == null) {
+      console.log("list is empty");
+    } else {
+      let temp = this.head;
+      if (temp.value != null) {
+        console.log(temp.value);
+      }
+      while (temp.next != null && temp.value != null) {
+        temp = temp.next;
+        console.log(temp.value);
+      }
+    }
+  }
   prepend(data) {
     let newNode = new Node(data);
-    let next1 = newNode.next;
-    console.log({next1})
-    console.log({ newNode });
-    console.log(this.head);
     if (this.head == null) {
       this.head = newNode;
-      console.log("ha",this.head)
     } else {
       newNode.next = this.head;
       this.head = newNode;
-      console.log("hello",this.head);
     }
+  }
+  size() {
+    let temp=this.head
+    let i = 0;
+    if (this.head == null) {
+      console.log("Size is 0");
+    } else {
+    while (temp != null) {
+      
+      i++
+      temp=temp.next
+      console.log(i)
+    }
+
+    }
+    // let temp=""
   }
 }
 
 L = new LinkedList();
 L.prepend(8);
-L.prepend(99)
+L.prepend(99);
+L.prepend(33);
+L.prepend(22);
+L.prepend(84);
 L.prepend(33)
+L.size()
+// L.printAll();
