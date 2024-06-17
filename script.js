a8671075d77db25fbc251009d2f71abd2ebe3d21
xonin-hush <xonindev@gmail.com>
2024-06-17 15:38:29 +0100

    Add prepend method

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
 class Node {
-    constructor(value,next)
-    {
-        this.value=null
-        this.value=null
-    }
+  constructor(value, next) {
+    this.value = value;
+    this.next = null;
+  }
 }
-class LinkedList{
-    constructor(head){
-        this.head=null
+class LinkedList {
+  constructor(head) {
+    this.head = null;
+  }
+  //   append(data) {
+  //     let temp = "";
+  //     let newNode=new Node(data)
+  //     console.log(newNode)
+  //     if (this.head == null) {
+  //       this.head.value = data;
+  //     } else {
+  //       if (this.head.next == null) {
+  //         this.head.next = data;
+  //       } else {
+  //         temp = this.head;
+  //         if (temp.next.next == null) {
+  //           temp.next.next = data;
+  //           console.log(temp.next.next);
+  //         }
+  //       }
+  //     }
+  //   }
+  prepend(data) {
+    let newNode = new Node(data);
+    let next1 = newNode.next;
+    console.log({next1})
+    console.log({ newNode });
+    console.log(this.head);
+    if (this.head == null) {
+      this.head = newNode;
+      console.log("ha",this.head)
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+      console.log("hello",this.head);
     }
-    
+  }
 }
+
+L = new LinkedList();
+L.prepend(8);
+L.prepend(99)
+L.prepend(33)
